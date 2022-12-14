84c3b255186ed0259c768de24b7232733ce816f4
dauid64 <rubiacarlalp56@gmail.com>
2022-12-14 00:21:07 -0300

    search

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -19,5 +19,32 @@ class SearchViewsTest(RecipeTestBase):
         self.assertEqual(response.status_code, 404)
 
     def test_search_term_is_on_page_title_and_escaped(self):
-        response = self.client.get(reverse('recipes:search') + '?search=<teste>')
-        self.assertIn('Search for &quot;&lt;teste&gt;&quot;', response.content.decode('utf-8'))
+        response = self.client.get(
+            reverse('recipes:search') + '?search=<teste>')
+        self.assertIn('Search for &quot;&lt;teste&gt;&quot;',
+                      response.content.decode('utf-8'))
+
+    def test_search_can_find_recipe_by_title(self):
+        title1 = 'This is recipe one'
+        title2 = 'This is recipe two'
+
+        recipe1 = self.make_recipe(
+            slug='one', title=title1, author_data={'username': 'one'})
+        recipe2 = self.make_recipe(
+            slug='two', title=title2, author_data={'username': 'two'})
+
+        response1 = self.client.get(
+            reverse('recipes:search') + f'?search={title1}')
+        response2 = self.client.get(
+            reverse('recipes:search') + f'?search={title2}')
+        response_both = self.client.get(
+            reverse('recipes:search') + f'?search=this')
+
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertNotIn(recipe2, response1.context['recipes'])
+
+        self.assertIn(recipe2, response2.context['recipes'])
+        self.assertNotIn(recipe1, response2.context['recipes'])
+
+        self.assertIn(recipe1, response_both.context['recipes'])
+        self.assertIn(recipe2, response_both.context['recipes'])
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from recipes.models import Recipe
+from django.db.models import Q
 from django.http import Http404
+from recipes.models import Recipe
 
 
 def home(request):
@@ -11,7 +12,6 @@ def home(request):
 def category(request, category_id):
     # recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id') # category_id é igual ao category_id da recipes # noqa: E501
     recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))   # noqa: E501
-
     return render(request, 'recipes/pages/category.html', context={'recipes': recipes, 'title': recipes[0].category.name})  # noqa: E501
 
 
@@ -25,4 +25,5 @@ def search(request):
     search_term = request.GET.get('search', '').strip()
     if not search_term:
         raise Http404()
-    return render(request, 'recipes/pages/search.html', {'page_title': f'Search for "{search_term}"'})
+    recipes = Recipe.objects.filter(Q(Q(title__icontains=search_term) | Q(description__icontains=search_term)), is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/search.html', {'page_title': f'Search for "{search_term}"', 'recipes': recipes})
