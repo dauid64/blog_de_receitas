fd970ba443a9d18d216618a2ad0226162fcff07e
dauid64 <rubiacarlalp56@gmail.com>
2022-12-13 23:18:58 -0300

    Refatorando tests

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -1,6 +1,7 @@
 from django.urls import resolve, reverse
 
 from recipes import views
+
 from .test_recipe_base import RecipeTestBase
 
 
@@ -16,3 +17,7 @@ class SearchViewsTest(RecipeTestBase):
     def test_search_raises_404_if_no_search_term(self):
         response = self.client.get(reverse('recipes:search'))
         self.assertEqual(response.status_code, 404)
+
+    def test_search_term_is_on_page_title_and_escaped(self):
+        response = self.client.get(reverse('recipes:search') + '?search=<teste>')
+        self.assertIn('Search for &quot;&lt;teste&gt;&quot;', response.content.decode('utf-8'))
--- recipes/views.py
+++ recipes/views.py
@@ -22,7 +22,7 @@ def recipe(request, id):
 
 
 def search(request):
-    search_term = request.GET.get('search')
+    search_term = request.GET.get('search', '').strip()
     if not search_term:
         raise Http404()
-    return render(request, 'recipes/pages/search.html')
+    return render(request, 'recipes/pages/search.html', {'page_title': f'Search for "{search_term}"'})
