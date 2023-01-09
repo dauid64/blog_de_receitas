65f7b8446d4ba24b0f6d8c43a5794f428c80c3ce
dauid64 <rubiacarlalp56@gmail.com>
2023-01-09 10:04:10 -0300

    responsive

--- project/settings/i18n.py
+++ project/settings/i18n.py
@@ -1,7 +1,14 @@
+from . import BASE_DIR
+
+
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
-USE_I18N = False
+USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale'
+]
